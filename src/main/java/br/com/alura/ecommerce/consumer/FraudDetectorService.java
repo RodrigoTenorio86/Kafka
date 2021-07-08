17ca15c5f41e1599f2d95d6bbd9c0c72d0e582c8
Rodrigo Tenorio de Boa Ventura <rodrigo.ventura@autbank.com.br>
2021-07-08 17:34:11 -0300

    Atualizacao: -Extraindo nossa camada de producer

--- src/main/java/br/com/alura/ecommerce/consumer/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/consumer/FraudDetectorService.java
@@ -38,14 +38,5 @@ public class FraudDetectorService {
 		System.out.println("Order processed");
 	}
 	
-	private static Properties properties() {
-		var properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-		
-		
-		return properties;
-	}
+
 }
--- src/main/java/br/com/alura/ecommerce/producer/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/producer/NewOrderMain.java
@@ -1,50 +1,26 @@
 package br.com.alura.ecommerce.producer;
 
-import java.util.Properties;
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 public class NewOrderMain {
 	
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		var producer = new KafkaProducer<String,String>(properties());
-		
-		Callback callback=(data, ex) ->{
-			
-				if(ex != null) {
-					ex.printStackTrace();
-					return;
-				}
-				System.out.println("Sucesso enviando "+data.topic()+ "::partition "+data.partition()+"/offset "+data.offset()+"/"+data.timestamp());
-			};
-			
+	  try(	var kafkaDispatcher= new KafkaDispatcher()){
+					
 		var key= UUID.randomUUID().toString();	
 		var value = "132123,67523,7894589745";
-		var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key,value);
 		
 		var email = "Obrigado voce por sua ordem! nos sao processo sua ordem!!!";
-		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",key,email);
-			
-		producer.send(record,callback).get();
-		producer.send(emailRecord,callback).get();
-	}
-
-
-	
-	private static Properties properties() {
-		var properties = new Properties();
-		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-			
-		return properties;
 		
-	}
+		
+		kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", key,value);
+					
+		kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL",key,email);
+	 }
 
+	}
+	
 }
